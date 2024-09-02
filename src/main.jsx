57914269b48c9dec775bdb160d2944713aaa4288
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 09:49:59 -0300

    elemento pai  e filhos criados das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+//import das dependencias
+
+//react-router-dom
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//routes
 import App from './App.jsx'
-import './index.css'
+import Error from './routes/Error.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Contato from './routes/Contato.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Home from './routes/Home.jsx'
+
+
+//criando a funcao Browser Router
+
+const router = createBrowserRouter([
+  {
+    //elemento pai
+
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+
+    //elemento filho
+
+    children:[
+      {path:'/', element:<Home/>},
+      {path:'/Contato', element:<Contato/>},
+      {path:'/', element:<Produtos/>},
+      {path:'/', element:<Sobre/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
