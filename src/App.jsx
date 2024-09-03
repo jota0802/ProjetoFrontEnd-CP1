74933b2bd2a44f27391eae45531722493ba67fe7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-03 09:09:53 -0300

    atualizando style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import {Outlet} from 'react-router-dom' 
-import './reset.css'
+import './global-styled.js'
 
 function App() {
 
