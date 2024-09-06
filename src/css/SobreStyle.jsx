03255c2e3ec752cdbb13136c1736b9287084c1df
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-06 09:03:31 -0300

    desining sobre

--- src/css/SobreStyle.jsx
+++ src/css/SobreStyle.jsx
@@ -1,7 +1,27 @@
 import styled from "styled-components"
 
 export const SobreStyle = styled.section `
-
-
+    width: 100%;
+    height: 90vh;
+    background-color: #ADB8BB;
+.Sobre{ 
+    margin-top: 18vh;
+    text-align: center;
+    padding: 2vh 18vh ;
+}
+.texto-sobre{
+    padding-top: 8vh;
+}
+h1{
+    font-size: 6vh;
+}
+h2{
+    font-size: 3.2vh;
+}
+p{
+    padding: 2.6vh;
+    font-size: 2vh;
+    line-height: 3vh;
+}
 
 `
